ea80dc3ac41c6555dd1af10671f0f8ebd3559688
Akira Youngblood <ayoungblood@users.noreply.github.com>
2025-01-09 19:49:32 -0700

    Add initial Ailunce HD1 files

---
+++ src/radios/ailunce_hd1.rs
@@ -0,0 +1,8 @@
+// src/radios/ailunce-hd1.rs
+
+// HD1(GPS) CPS v3.03
+// HD1 CPS is pretty cursed and only supports export/import of the following:
+// * Channels
+// * Contacts
+// * Priority Contacts
+// This implies that most codeplugs will take quite a lot of manual fixing-up
