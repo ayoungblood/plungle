b1cfdce102f0d87c55190bd95574382fa3c81b33
Akira Youngblood <ayoungblood@users.noreply.github.com>
2025-02-27 18:32:05 -0700

    WIP, testing XML parsing

--- src/radios/mod.rs
+++ src/radios/mod.rs
@@ -16,6 +16,7 @@ mod opengd77_rt3s;
 mod chirp_generic;
 mod ailunce_hd1;
 mod alinco_djmd5t;
+mod motorola_xpr7550;
 
 pub fn parse_codeplug(model: &String, input: &PathBuf, opt: &Opt) -> Result<Codeplug, Box<dyn Error>> {
     uprintln!(opt, Stderr, None, 2, "{}:{}()", file!(), function!());
@@ -27,6 +28,7 @@ pub fn parse_codeplug(model: &String, input: &PathBuf, opt: &Opt) -> Result<Code
     read_functions.insert("chirp_generic", chirp_generic::read);
     read_functions.insert("ailunce_hd1", ailunce_hd1::read);
     read_functions.insert("alinco_djmd5t", alinco_djmd5t::read);
+    read_functions.insert("motorola_xpr7550", motorola_xpr7550::read);
 
     // look up the radio model in the hashmap
     if let Some(read_function) = read_functions.get(model.as_str()) {
